b0da3460426222315a6f2059d84e532f51f71289
Suman Mondal <76463576+sumanmondal1994@users.noreply.github.com>
2026-02-05 05:53:14 +1100

    Initial Code commit

---
+++ src/main/java/com/project/packingoptions/PackingOptionsApplication.java
@@ -0,0 +1,13 @@
+package com.project.packingoptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackingOptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackingOptionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/packingoptions/PackingOptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.packingoptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackingOptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
